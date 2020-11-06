8834252f6d0533a6a889bc42050935396d22464a
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-06 14:01:23 +0900

    messagesテーブルの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
+    @room= Room.find(params[:room_id])
+    @message= Message.new
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
